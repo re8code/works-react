4b4ec9a1bbea3c91b3ad83afb287f150fccd84f6
re8code <won@re8code.com>
2024-08-11 23:21:55 +0900

    add element1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Element1 from './Element1.js';
 
 function App() {
   return (
     <>
-    <h1>Hello, React!</h1>
+    <Element1 />
     </>
   );
 }
---
+++ src/Element1.js
@@ -0,0 +1,11 @@
+function element () {
+  return (
+    <div style={{width:'1000px', margin:'0 auto'}}>
+      <h1>Element1</h1>
+      <hr />
+      <p>Element1 is function type.</p>
+    </div>
+  );
+}
+
+export default element;
