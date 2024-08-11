1cd65e57f1a661ffc3eb7362c2e51c84e6c52f0f
re8code <won@re8code.com>
2024-08-12 00:35:11 +0900

    add element2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import './main.css';
 import Element1 from './Element1.js';
+import Element2 from './Element2.js';
 
 function App() {
   return (
     <>
     <Element1 />
+    <div className='box_2nd'>
+      <h1>Element2</h1>
+      <hr />
+      <Element2 />
+      <Element2 />
+      <Element2 />
+      <Element2 />
+    </div>
     </>
   );
 }
---
+++ src/Element2.js
@@ -0,0 +1,13 @@
+import './main.css';
+
+const element = () => {
+  return (
+    <p className='ele2_p'>
+      <span>element title</span>
+      <br/>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  );
+}
+
+export default element;
